f8f52091cefc76aa1f6dab7b1a1b56c185a35729
coSK123 <62240598+coSK123@users.noreply.github.com>
2025-01-22 16:10:14 +0100

    fixed cors

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -3,6 +3,10 @@ const allowedOrigins = [
     'http://localhost:4100',
     'http://localhost:80',
     'http://localhost:3306',
+    'http://141.72.188.181:4100',
+    'http://141.72.188.181:80',
+    'http://141.72.188.181:3306'
+    
 ]
 
 export default allowedOrigins;
