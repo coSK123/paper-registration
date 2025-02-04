3beb2d5d38cc0cb6fa60bb3f902cdce0285839d0
Sebastian Kollmer <62240598+coSK123@users.noreply.github.com>
2025-02-04 21:26:20 +0100

    added user entrypoint via login

--- Frontend/src/app/services/current-user/current-user.service.ts
+++ Frontend/src/app/services/current-user/current-user.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 export interface User {
   email: string;
   role: string;
 }
+
 @Injectable({
   providedIn: 'root',
 })
 export class CurrentUserService {
+  private loggedIn = new BehaviorSubject<boolean>(this.isUserLoggedIn());
+  loginStatusChanged = this.loggedIn.asObservable();
+
   constructor() {}
 
   saveUser(user: User): void {
     localStorage.setItem('user', JSON.stringify(user));
+    this.loggedIn.next(true); // Notify subscribers that the user is logged in
   }
 
   removeUser(): void {
     localStorage.removeItem('user');
+    this.loggedIn.next(false); // Notify subscribers that the user is logged out
   }
 
   getUser(): User | null {
     const user = localStorage.getItem('user');
     return user ? JSON.parse(user) : null;
+  }
+
+  isUserLoggedIn(): boolean {
+    return this.getUser() !== null;
   }
 
   isAdmin(): boolean {
