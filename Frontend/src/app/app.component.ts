edb1a5d85b5cb67cbd38abd3eec3fa910310644d
Sebastian Kollmer <62240598+coSK123@users.noreply.github.com>
2025-02-09 17:00:39 +0100

    fixed login update error

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,25 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './sharedComponents/header/header.component';
 import { CurrentUserService } from './services/current-user/current-user.service';
 import { LoginComponent } from './login/login.component';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Frontend';
 
-  isLoggedIn = false;
+  isLoggedIn$:Observable<boolean>;
 
-  constructor(private currentUser: CurrentUserService, private cdr: ChangeDetectorRef) {
-    this.isLoggedIn = this.currentUser.isUserLoggedIn();
-
-    // Subscribe to login status changes
-    this.currentUser.loginStatusChanged.subscribe((isLoggedIn: boolean) => {
-      this.isLoggedIn = isLoggedIn;
-      this.cdr.detectChanges(); // Trigger change detection
-    });
+  constructor(private currentUser: CurrentUserService) {
+    this.isLoggedIn$ = this.currentUser.loginStatusChanged;
   }
 }
