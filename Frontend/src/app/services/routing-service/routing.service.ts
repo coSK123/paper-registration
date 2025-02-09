abc44cfc7b1ef7f1fcccc60c4212fba142c149dc
Sebastian Kollmer <62240598+coSK123@users.noreply.github.com>
2025-02-09 17:01:10 +0100

    implemented role based routing

---
+++ Frontend/src/app/services/routing-service/routing.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CurrentUserService } from '../current-user/current-user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoutingService {
+  constructor(private currentUserService: CurrentUserService) {}
+
+  routeAfterLogin() {
+    if (this.currentUserService.isAdmin()) {
+      return '/a/dashboard';
+    } else if (this.currentUserService.isProfessor()) {
+      return '/d/dashboard';
+    } else if (this.currentUserService.isStudent()) {
+      return '/s/dashboard';
+    } else {
+      return '/login';
+    }
+  }
+}
