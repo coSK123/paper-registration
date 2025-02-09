75fa6c0381a5e9e07a914cdb01196f848af60f44
Sebastian Kollmer <62240598+coSK123@users.noreply.github.com>
2025-02-09 20:31:02 +0100

    Created navigation bar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './sharedComponents/header/header.component';
 import { CurrentUserService } from './services/current-user/current-user.service';
 import { LoginComponent } from './login/login.component';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './sharedComponents/navigation-bar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginComponent, CommonModule],
+  imports: [ HeaderComponent, LoginComponent, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
