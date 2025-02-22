47cc37b8149d39739eef50b003cadeef6ddb3998
coSK123 <62240598+coSK123@users.noreply.github.com>
2025-02-22 15:42:33 +0100

    changes semester selection to autocomplete input

--- Frontend/src/app/admin-dashboard/active-semester/active-semester.component.ts
+++ Frontend/src/app/admin-dashboard/active-semester/active-semester.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { map, Observable, startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-active-semester',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatCardModule],
+  imports: [MatFormFieldModule, MatAutocompleteModule, MatInputModule, FormsModule, MatCardModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './active-semester.component.html',
   styleUrl: './active-semester.component.scss'
 })
-export class ActiveSemesterComponent {
+export class ActiveSemesterComponent{
+myControl = new FormControl('');
+filteredOptions: Observable<string[]>;
+
 semesters = ["W22", "S22", "W23", "S23"]
-selectedSemester = "W22"
+currentlyActiveSemester: string
+
+constructor() {
+  this.currentlyActiveSemester = "W22"
+  this.filteredOptions = this.myControl.valueChanges.pipe(
+    startWith(''),
+    map(value => this._filter(value || '')),
+  );
+}
+
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.semesters.filter(semester => semester.toLowerCase().includes(filterValue));
+  }
 }
