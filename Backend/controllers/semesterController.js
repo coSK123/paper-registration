a3ffe495ad60958729c6c207168f67a5618e9a0c
coSK123 <62240598+coSK123@users.noreply.github.com>
2025-02-22 16:14:20 +0100

    created semesterController for the backend

---
+++ Backend/controllers/semesterController.js
@@ -0,0 +1,45 @@
+import Semester from "../model/semester";
+
+const getActiveSemester = async (req, res) => {
+    try {
+        
+        const activeSemester = await Semester.findOne({ where: { active: true } });
+        if(activeSemester){
+            res.json(activeSemester);
+        } else{
+            res.json({name:"", active:false});
+        }
+        
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+const getSemesters = async (req, res) => {
+    try {
+        const semesters = await Semester.findAll();
+        res.json(semesters);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+const activateSemester = async (req, res) => {
+    semester = req.body;
+    try{
+        let databaseSemester = await Semester.findOne({ where: { name:semester } })
+        if(!databaseSemester){
+            await Semester.update({active:false}, {where:{active:true}})
+            await Semester.create({name:semester, active:true})
+        } else{
+            await Semester.update({active:false}, {where:{active:true}})
+            await databaseSemester.update({active:true})
+        }
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+
+}
+
+export default { getActiveSemester, getSemesters, activateSemester };
